bdd488fae8d6c38a3946c1a423b68deca8072a0d
oggysy <ogisoys@gmail.com>
2023-02-28 21:49:09 +0900

    ロープのドラッグ移動と元の位置に戻るアニメーション実装

--- unoji/ViewController.swift
+++ unoji/ViewController.swift
@@ -8,12 +8,61 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var ropeView: UIView!
+    @IBOutlet weak var ropeViewTopLayoutConstraint: NSLayoutConstraint!
+    private var ropeViewStartPosition: CGFloat?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        //ropeの初期位置をropeViewStartPosition変数に入れておく
+        ropeViewStartPosition = ropeViewTopLayoutConstraint.constant
+        //UIPanGestureRecognizerを初期化して変数に格納し、ropeViewに渡す
+        let panGesture = UIPanGestureRecognizer(
+            target: self,
+            action: #selector(didPan(_:))
+        )
+        ropeView.addGestureRecognizer(panGesture)
     }
-
-
+    
+    //スワイプで呼ばれる関数
+    @objc
+    func didPan(_ recognizer: UIPanGestureRecognizer) {
+        //ropeの初期位置をアンラップ
+        guard let startPositon = ropeViewStartPosition else { return }
+        //スワイプの移動量を変数pointに格納
+        let point: CGPoint = recognizer.translation(in: self.view)
+        //現在のropeの位置＋y軸の移動量を足して予測されるtop位置を変数expectedRopePositonに格納
+        let expectedRopePositon = ropeViewTopLayoutConstraint.constant + point.y
+        //ropeが下に移動する最大のtop位置を変数に格納
+        let ropeMaxLength: CGFloat = -70
+        
+        //予測される移動位置がropeMaxLengthを超えていなければスワイプ量を現在位置に足す
+        //超える場合はropeMaxLengthに指定
+        if expectedRopePositon <= ropeMaxLength {
+            ropeViewTopLayoutConstraint.constant += point.y
+//            print(ropeViewTopLayoutConstraint.constant)
+            recognizer.setTranslation(CGPoint.zero, in: self.view)
+        } else {
+            ropeViewTopLayoutConstraint.constant = ropeMaxLength
+        }
+        
+        //スワイプ終了時に呼ばれる処理
+        if recognizer.state == .ended {
+            //もしスワイプ終了時にropeの位置がropeMaxLengthになっていれば水が流れるアニメーションを実行
+            if ropeViewTopLayoutConstraint.constant == ropeMaxLength {
+                flushWater()
+            }
+            //ropeを初期位置に戻し、animationしながら元の位置に戻る
+            self.ropeViewTopLayoutConstraint.constant = startPositon
+            UIView.animate(withDuration: 0.3, delay: 0, usingSpringWithDamping: 0.3, initialSpringVelocity: 0.5) {
+                self.view.layoutIfNeeded()
+            }
+        }
+    }
+    
+    //ここにLottieAnimationを実行する処理を書いていく
+    private func flushWater() {
+        print("Flush Water")
+    }
 }
-
